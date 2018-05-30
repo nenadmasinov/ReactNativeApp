85e708c64d51aacddc553383ae2b57f25ecb574f
Nenad Masinov <nenad@blafsoft.com>
2018-05-30 13:18:55 +0200

    changes

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import SplashScreen from './Components/SplashScreen/SplashScreen'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! Test Masinov</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <SplashScreen />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
